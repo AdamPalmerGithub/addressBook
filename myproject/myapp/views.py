37d907ee15720a59bf40ddc840e2763e74d2e551
Miller <millershaw61@gmail.com>
2025-01-30 12:42:55 +0000

    Feat: Templates, urls and views added.
    Landing page, and login page drafted.

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    return render(request,"myapp/index.html",{})
+
+def login(request):
+    return render(request, "myapp/login.html",{})
+
+@login_required
+def app(request):
+    pass
