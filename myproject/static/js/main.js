18b7e9bcf65436eff6b567677ace5a7c51959146
Miller <millershaw61@gmail.com>
2025-02-06 11:07:37 +0000

    Feat (login): Light and dark mode
    Beginning migration to seperate css files, and beginning integration of dark/light mode for addressBook page.

---
+++ myproject/static/js/main.js
@@ -0,0 +1,11 @@
+function light() {
+   var element = document.body;
+   element.className = ""
+   element.classList.add("light")
+}
+function dark() {
+   var element = document.body;
+   element.className = ""
+   element.classList.add("dark")
+}
+
