be8be6ea5d7bf16a148bebf31a5bd89a2b3242b0
Miller <millershaw61@gmail.com>
2025-02-14 12:50:52 +0000

    Styles: Form input improvements.

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -2,12 +2,65 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    first_name = forms.CharField(max_length=100, required=True, label="First Name")
-    last_name = forms.CharField(max_length=100, required=True, label="Last Name")
-    email_address = forms.EmailField(required=False, label="Email")
-    phone_number = forms.CharField(max_length=15, required=False, label="Phone Number")
-    postcode = forms.CharField(max_length=20, required=False, label="Postcode")
-    tags = forms.CharField(widget=forms.TextInput(), required=False, label="Tags (comma-separated)")
+    first_name = forms.CharField(
+        max_length=100,
+        required=True,
+        label="First Name",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        required=True,
+        label="Last Name",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    email_address = forms.EmailField(
+        required=False,
+        label="Email",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    phone_number = forms.CharField(
+        max_length=15,
+        required=False,
+        label="Phone Number",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    postcode = forms.CharField(
+        max_length=20,
+        required=False,
+        label="Postcode",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    tags = forms.CharField(
+        required=False,
+        label="Tags (comma-separated)",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+
 
 class UserUpdateForm(forms.Form):
     username = forms.CharField(max_length=20, required=True, label="Username")
@@ -27,20 +80,74 @@ class UserUpdateForm(forms.Form):
         )
     )
 
+
 class ContactUpdateForm(forms.Form):
-    first_name = forms.CharField(max_length=100, required=True, label="First Name")
-    last_name = forms.CharField(max_length=100, required=True, label="Last Name")
-    email_address = forms.EmailField(required=False, label="Email")
-    phone_number = forms.CharField(max_length=15, required=True, label="Phone Number")
-    postcode = forms.CharField(max_length=20, required=False, label="Postcode")
-    tags = forms.CharField(widget=forms.TextInput(), required=False, label="Tags (comma-separated)")
+    first_name = forms.CharField(
+        max_length=100,
+        required=True,
+        label="First Name",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        required=True,
+        label="Last Name",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    email_address = forms.EmailField(
+        required=False,
+        label="Email",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    phone_number = forms.CharField(
+        max_length=15,
+        required=True,
+        label="Phone Number",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    postcode = forms.CharField(
+        max_length=20,
+        required=False,
+        label="Postcode",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+    tags = forms.CharField(
+        required=False,
+        label="Tags (comma-separated)",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-inputs',
+            }
+        )
+    )
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=20,
         required=True,
         label="Username",
-        widget = forms.TextInput(
+        widget=forms.TextInput(
             attrs={
                 'placeholder': 'Enter your username',
                 'class': 'form-inputs',
@@ -51,7 +158,7 @@ class LoginForm(forms.Form):
         max_length=50,
         required=True,
         label="Password",
-        widget = forms.PasswordInput(
+        widget=forms.PasswordInput(
             attrs={
                 'placeholder': 'Enter your password',
                 'class': 'form-inputs',
@@ -67,4 +174,3 @@ class SignUpForm(forms.Form):
     email = forms.EmailField(required=True, label="Email")
     phone_number = forms.CharField(max_length=15, required=True, label="Phone Number")
     password = forms.CharField(max_length=50, required=True, widget=forms.PasswordInput, label="Password")
-    
